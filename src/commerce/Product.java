c9a6fe0e1c8d4a26fd9522cdad6f6273dd0f10ad
Jiwon Yang <g.one9709@gmail.com>
2026-03-26 16:31:03 +0900

    feature: step1 1. Product 클래스 생성 - 상품 속성, 생성자, 기능 추가 - 상품 속성: 상품명, 가격, 설명, 재고수량 - 기능: getter
    2. Main 클래스
    -Product 인스턴스화
    -List 선언 -> 상품 추가

--- src/commerce/Product.java
+++ src/commerce/Product.java
@@ -1,4 +1,28 @@
 package commerce;
 
 public class Product {
+
+    // 속성
+    // 상품명, 가격, 설명, 재고수량
+    private String productName;
+    private int productPrice;
+    private String productDescription;
+    private int productStock;
+
+    //생성자
+    public Product(String productName, int productPrice, String productDescription, int productStock) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productDescription = productDescription;
+        this.productStock = productStock;
+    }
+
+
+    //기능
+
+    // 1. getter
+
+    public String getProductName() {
+        return productName;
+    }
 }
