6542d4a2462d9ef4e6c8f2e06265b28954803792
Jiwon Yang <g.one9709@gmail.com>
2026-03-26 16:44:29 +0900

    CommerceSystem 클래스 생성 및 Product 관리 리스트

--- src/commerce/CommerceSystem.java
+++ src/commerce/CommerceSystem.java
@@ -1,4 +1,20 @@
 package commerce;
 
+// 커머스 플랫폼의 상품을 관리하고 사용자 입력을 처리하는 클래스
+
+import java.util.List;
+
 public class CommerceSystem {
+
+    // 속성
+    List<Product> productList; // Product를 관리하는 리스트
+
+
+    // 생산자
+
+
+
+    // 기능
+
+
 }
