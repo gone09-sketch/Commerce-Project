d2e59d8415d48e9148c66d3aef8da60192bcb8aa
Jiwon Yang <g.one9709@gmail.com>
2026-03-27 18:02:16 +0900

    feat: Customer 클래스 및 속성 (고객명, 이메일, 등급) 생성

---
+++ src/commerce/Customer.java
@@ -0,0 +1,18 @@
+package commerce;
+
+// 고객 관리
+
+public class Customer {
+
+    //속성
+    private String customerName; //고객명
+    private String customerEmail; // 이메일
+    private String customerLevel; // 등급
+
+    //생성자
+
+
+    //기능
+
+
+}
