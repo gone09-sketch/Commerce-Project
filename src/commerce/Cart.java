08b128f338cbe8d07d45b8ff4672ebb74d8a605e
Jiwon Yang <g.one9709@gmail.com>
2026-03-30 15:08:00 +0900

    refactor: step3 commerce 1. Category 클래스 -입출력 삭제 -productList 반환 메서드 구현
    2. CommerceSystem
    -productList 출력
    -[" " 카테고리] 메뉴에서 사용자 입력값 받기

---
+++ src/commerce/Cart.java
@@ -0,0 +1,20 @@
+package commerce;
+
+// 장바구니
+
+public class Cart {
+
+    // 속성
+    // 장바구니 리스트 List<Cart>? -> 선택된 상품들
+    // 재고 관리 -> productStock
+    // 입력 스캐너
+
+
+
+
+    // 생성자
+
+
+
+    // 기능
+}
--- src/commerce/Category.java
+++ src/commerce/Category.java
@@ -20,37 +20,14 @@ public class Category {
 
 
     // 기능
-    // 1. 카테고리 이름을 반환하는 메서드
+    // 1. categoryName 반환 메서드
     public String getCategoryName() {
         return categoryName;
     }
 
-    // 2. 선택된 카테고리 출력
-    public void printCategory() {
-        while (true) {
-            int productNumber = 1;
-            System.out.println("[ " + this.categoryName + " 카테고리 ]");
-            for (Product product : productList) {
-                System.out.println(String.format("%d. %-12s | %,10d원 | %s",
-                        productNumber,
-                        product.getProductName(),
-                        product.getProductPrice(),
-                        product.getProductDescription()));
-                productNumber++;
-            }
-            System.out.println(String.format("%d. %-11s", 0, "뒤로가기"));
-            break;
-        }
-    }
-
-    // 3. productList 개수 반환 메서드
-    public int getProductListCount() {
-        return productList.size();
-    }
-
-    // 4. productList의 상품 1개를 반환하는 메서드
-    public Product getProduct(int index) {
-        return productList.get(index);
+    // 2. productList 반환 메서드
+    public List<Product> getProductList() {
+        return productList;
     }
 
 
